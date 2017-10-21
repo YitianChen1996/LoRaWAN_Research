8f5b6a67e70461a089a34321e9981b809f02b5e1
YitianChen1996 <30911432+YitianChen1996@users.noreply.github.com>
2017-10-21 17:29:07 -0400

    Update IEEEConvert.cpp

--- GreenBus/IEEEConvert.cpp
+++ GreenBus/IEEEConvert.cpp
@@ -1,28 +1,41 @@
 #include <iostream>
 #include <string.h>
 
-void changeDoubletoIEEE(double *dp, char *&ret) {
-    char *cp;
-    cp = (char *) dp;
+void changeDoubletoIEEE(double *dp, unsigned char *&ret) {
+    unsigned char *cp;
+    cp = (unsigned char *) dp;
+    unsigned char temp[8];
     for (int i = 0; i <= 7; i++) {
-        strcpy(ret + i, cp + i);
+        temp[i] = cp[i];
+    }
+    for (int i = 0; i <= 7; i++) {
+        unsigned char tempprint[2];
+        sprintf((char *) tempprint, "%02x", temp[i]);
+        strcat((char *) ret, (char *) tempprint);
     }
 }
 
-double changeIEEEtoDouble(char *IEEE) {
-    double *pd;
-    pd = (double *) IEEE;
+double changeIEEEtoDouble(unsigned char *IEEE) {
+    double *pd, module;
+    unsigned char *pc;
+    pd = &module;
+    pc = (unsigned char *) &module;
+    for (int i = 0; i <= 15; i++) {
+        if (IEEE[i]=='0'&&i%2==0){
+            *pc=0x0000;
+        }
+    }
     return *pd;
 }
 
 int main() {
     double a = -75.1235432;
-    char *ac;
-    ac = (char *) malloc(8 * sizeof(char));
-    memset(ac, 0, 8 * sizeof(char));
+    unsigned char *ac;
+    ac = (unsigned char *) malloc(16 * sizeof(char));
+    memset(ac, 0, 16 * sizeof(char));
     changeDoubletoIEEE(&a, ac);
-    for (int i = 0; i <= 7; i++) {
-        printf("%02x ", ac[i]);
+    for (int i = 0; i <= 15; i++) {
+        printf("%c ", ac[i]);
     }
     printf("\n");
     printf("%lf\n", changeIEEEtoDouble(ac));
