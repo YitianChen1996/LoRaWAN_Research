94a63de77acd6c2ab2ec77b1ed8635746814ccec
YitianChen1996 <30911432+YitianChen1996@users.noreply.github.com>
2017-10-21 18:13:39 -0400

    Update IEEEConvert.cpp

--- GreenBus/IEEEConvert.cpp
+++ GreenBus/IEEEConvert.cpp
@@ -10,8 +10,47 @@ void changeDoubletoIEEE(double *dp, unsigned char *&ret) {
     }
     for (int i = 0; i <= 7; i++) {
         unsigned char tempprint[2];
-        sprintf((char *) tempprint, "%02x", temp[i]);
+        sprintf((char *) tempprint, "%02X", temp[i]);
         strcat((char *) ret, (char *) tempprint);
+    }
+}
+
+unsigned short changChartoShort(char c) {
+    switch (c) {
+        case '0':
+            return 0;
+        case '1':
+            return 1;
+        case '2':
+            return 2;
+        case '3':
+            return 3;
+        case '4':
+            return 4;
+        case '5':
+            return 5;
+        case '6':
+            return 6;
+        case '7':
+            return 7;
+        case '8':
+            return 8;
+        case '9':
+            return 9;
+        case 'A':
+            return 10;
+        case 'B':
+            return 11;
+        case 'C':
+            return 12;
+        case 'D':
+            return 13;
+        case 'E':
+            return 14;
+        case 'F':
+            return 15;
+        default:
+            return 0x0000;
     }
 }
 
@@ -20,11 +59,14 @@ double changeIEEEtoDouble(unsigned char *IEEE) {
     unsigned char *pc;
     pd = &module;
     pc = (unsigned char *) &module;
-    for (int i = 0; i <= 15; i++) {
-        if (IEEE[i]=='0'&&i%2==0){
-            *pc=0x0000;
-        }
+    memset(pd, 0, 8 * sizeof(unsigned char));
+    for (int i = 0; i <= 7; i++) {
+        *pc = 0;
+        *pc = (unsigned char)(changChartoShort(IEEE[2 * i]) << 4);
+        *pc = *pc + (unsigned char) changChartoShort(IEEE[2 * i + 1]);
+        pc++;
     }
+    pc = (unsigned char *) &module;
     return *pd;
 }
 
